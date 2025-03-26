78b897147f41f7be3ccf71ab49a7b651858a3b7a
AdrienVerwaerde <verwaerdeadrien@gmail.com>
2025-03-26 11:43:57 +0100

    about page ok

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  const AboutPage({Key? key}) : super(key: key);
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('À Propos')),
+      body: 
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text(
+              'COUCOU',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            Image.asset('images/about.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
